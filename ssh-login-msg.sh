e682dc4aa8eb382718bc4826df5c668d8a9e66b3
Olivetti <1502093+Olivetti@users.noreply.github.com>
2024-02-10 17:51:41 +0100

    v1.0

--- ssh-login-msg.sh
+++ ssh-login-msg.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 #set -vux
 #
-# ssh-login-msg
+# ssh-login-msg.sh
 # send message on ssh login via messenger bot
 # © Olivetti 2020-2024
 #
 # dependencies: curl
-# ipinfo.io?token=: no use of token
+#
+# mostly we were sourced (${0} -> ${BASH_SOURCE} / exit -> return)
 
 version="v1.0"
 
@@ -14,41 +15,52 @@ version="v1.0"
 
 [[ -n "${SSH_CLIENT}" ]] && {
 
- isodate=$(date +'%FT%T%z')
- logfile=~/"${0##*/}.log" # ~/ssh-login-msg.log
+   [[ -n "${BASH_SOURCE}" ]] && myname="${BASH_SOURCE[0]##*/}" || myname="${0##*/}"
 
-  ip_srv=$(hostname -I		| awk '{print $1}')
-  ip_usr=$(echo "${SSH_CLIENT}"	| awk '{print $1}')
+   isodate=$(date +'%FT%T%z')
+   logfile=~/"${myname}.log" # ~/ssh-login-msg.sh.log
 
-  ipinfo=$(curl -s "https://ipinfo.io/${ip_usr}")
+    ip_srv=$(hostname -I		| awk '{print $1}')
+    ip_usr=$(echo "${SSH_CLIENT}"	| awk '{print $1}')
 
-  ipinfo=$(echo "${isodate}" && echo "${ipinfo}" | sed -e '/^[{}]/d' -e 's/^  //' -e '/readme.*missingauth/d' -e 's/"//g' -e 's/,$//g')
-     log=$(echo $(echo "${ipinfo}"))
-     echo "${log}" >>"${logfile}"
+ tokenfile=~/.config/ipinfo/config.json
+ [[ -f "${tokenfile}" ]] && token=$(sed -Ee 's/.*,"token":"(.*)",.*/\1/' "${tokenfile}")
+
+    ipinfo=$(curl -s "https://ipinfo.io/${ip_usr}?token=${token}")
+
+    ipinfo=$(echo "${isodate}" && echo "${ipinfo}" | sed -e '/^[{}]/d' -e 's/^  //' -e '/readme.*missingauth/d' -e 's/"//g' -e 's/,$//g')
+       log=$(echo $(echo "${ipinfo}"))
+       echo "${log}" >>"${logfile}" # writing logfile
 
  send_telegram() {
   # telegram
-  # telegram_bot_token="" # not used anymore
-  # telegram_chat_id=""   # not used anymore
-  # telegram bot credentials in ~/.ssh-login-msg.env # use chmod 600
-  source ~/".${0##*/}.env"
+  # telegram_bot_token="" # we use env file
+  # telegram_chat_id=""   # we use env file
+  # telegram bot credentials in ~/.ssh-login-msg.sh.env # use chmod 600
+
+  _error() { echo "Error: Telegram - ${1}" && return; }
+
+  [[ -f ~/".${myname}.env" ]] && source ~/".${myname}.env" || _error "No env file"
+
   [[ -n "${telegram_bot_token}" ]] && [[ -n "${telegram_chat_id}" ]]			&& \
 	telegram_url="https://api.telegram.org/bot${telegram_bot_token}/sendMessage"	|| \
-		{ echo "Error: Telegram - No token/chat_id" && exit 1; }
+		_error "No token/chat_id"
 
   ip_padding() { echo "${1}" | sed 's/[0-9]\{1,\}/00&/g;s/0*\([0-9]\{3,\}\)/\1/g'; }
   ip_srv_pad=$(ip_padding "${ip_srv}")
   ip_usr_pad=$(ip_padding "${ip_usr}")
 
   url_encode() { echo "${1}" | sed -e 's/+/%2b/g' -e 's/&/%26/g' -e 's/</%3c/g' -e 's/>/%3e/g'; }
-      ipinfo=$(url_encode "${ipinfo}")
      isodate=$(url_encode "${isodate}")
+      ipinfo=$(url_encode "${ipinfo}")
 
-  msg="<pre>SSH Login</pre><code>Date: ${isodate}%0aUser: ${USER}@${HOSTNAME}%0aHost: ${ip_srv_pad}%0aFrom: ${ip_usr_pad}</code><pre>${ipinfo}</pre>"
+  msg="<pre>SSH Login ${USER}@${HOSTNAME}</pre><code>Date: ${isodate}%0aUser: ${USER}@${HOSTNAME}%0aHost: ${ip_srv_pad}%0aFrom: ${ip_usr_pad}</code> \
+	<pre>${ipinfo}</pre>"
 
-  curl -s --connect-timeout 10 -d "chat_id=${telegram_chat_id}&disable_web_page_preview=1&parse_mode=HTML&text=${msg}" "${telegram_url}" >/dev/null
+  curl -s --connect-timeout 10 -d "chat_id=${telegram_chat_id}&disable_web_page_preview=1&parse_mode=HTML&text=${msg}" \
+	"${telegram_url}" >/dev/null
  }
 
- send_telegram
+ send_telegram &
 
 }
