6719cc98baa12d99e177d713adef9da5b065eabe
Olivetti <1502093+Olivetti@users.noreply.github.com>
2025-08-10 01:11:48 +0200

    update: rumble-settings v1.0

--- rumble-settings.js
+++ rumble-settings.js
@@ -1,29 +1,30 @@
 // ==UserScript==
 // @name        rumble - Settings
-// @version     1.0
 // @description rumble - Settings
-// @author      Olivetti
+// @version     1.0
 // @namespace   Violentmonkey Scripts
 // @match       https://rumble.com/*
 // @icon        https://www.google.com/s2/favicons?sz=64&domain=rumble.com
-// @grant       none
-// @license     MIT
 // @downloadURL https://github.com/Olivetti/public/raw/master/rumble-settings.js
 // @updateURL   https://github.com/Olivetti/public/raw/master/rumble-settings.js
+// @supportURL  https://github.com/Olivetti/public
+// @author      Olivetti
+// @grant       none
+// @license     MIT
 // ==/UserScript==
 
-// theme: light, dark, system
+// theme: dark
 // collapse menu: true
 
 let attr1  = 'data-theme';
 let value1 = 'dark';
 
 let attr2  = 'class';
-let value2 = value1+' main-menu-mode-permanent';
+let value2 = 'main-menu-mode-permanent '+value1;
 
-try{
+try {
   document.documentElement.setAttribute(attr1,value1);
   document.documentElement.setAttribute(attr2,value2);
-}catch{/*do nothing*/}
-
-//alert(document.documentElement.getAttribute(attr1));
+} catch(error) {
+  console.error(error);
+}
